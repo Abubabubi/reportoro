5ae5121d7a49106a756b8f17a688f6b8d2c8a781
Anton Komissarov <anton.kom@hotmail.com>
2018-12-11 23:54:41 +0200

    Introduce SpringBoot

---
+++ src/core/src/main/com/antonkom/reportoro/ReportoroApplication.kt
@@ -0,0 +1,11 @@
+package com.antonkom.reportoro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportoroApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReportoroApplication>(*args)
+}
---
+++ src/core/src/test/com/antonkom/reportoro/ReportoroApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.antonkom.reportoro
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ReportoroApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
