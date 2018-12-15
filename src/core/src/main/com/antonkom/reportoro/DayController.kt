2c979bf72941fa28c9ac23c1bcf366317abc0451
Anton Komissarov <anton.kom@hotmail.com>
2018-12-16 01:49:28 +0200

    Stray away from spring tutorial; Start implementing own REST Api

--- src/core/src/main/com/antonkom/reportoro/DayController.kt
+++ src/core/src/main/com/antonkom/reportoro/DayController.kt
@@ -1,17 +1,16 @@
 package com.antonkom.reportoro
 
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class DayController {
+class DayController(private val repository: ArticleRepository) {
 
     @GetMapping("/")
-    fun day(model: Model): String {
-        model["title"] = "Day"
-        return "day"
+    fun day(model: Model): ResponseEntity<Iterable<Article>> {
+        val article = repository.findAllByOrderByAddedAtDesc()
+        return ResponseEntity.ok().body(article)
     }
-
 }
