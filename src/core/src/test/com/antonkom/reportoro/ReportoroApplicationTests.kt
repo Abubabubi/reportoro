f8b268d3df3c2d4f5188a585fd9f8d1b3bc15e11
Anton Komissarov <anton.kom@hotmail.com>
2018-12-16 00:33:15 +0200

    Enable @BeforeAll @AfterAll support in JUnit

--- src/core/src/test/com/antonkom/reportoro/ReportoroApplicationTests.kt
+++ src/core/src/test/com/antonkom/reportoro/ReportoroApplicationTests.kt
@@ -16,10 +16,25 @@ import java.net.URI
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
     @Test
     fun `Assert blog page title, content and status code`() {
         val entity = restTemplate.getForEntity<String>(URI.create("/"), String::class.java)
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).contains("<h1>Day</h1>")
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
     }
 }
