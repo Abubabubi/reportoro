bdb2568f214e11691ff59739c8cc9ce98965ad48
Anton Komissarov <anton.kom@hotmail.com>
2018-12-15 16:20:10 +0200

    Initial test configuration

--- src/core/src/test/com/antonkom/reportoro/ReportoroApplicationTests.kt
+++ src/core/src/test/com/antonkom/reportoro/ReportoroApplicationTests.kt
@@ -1,13 +1,14 @@
 package com.antonkom.reportoro
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
-class ReportoroApplicationTests {
+class BlogApplicationTests {
 
     @Test
     fun contextLoads() {
