df82a12b02ac70e31e9b6a4601ed629b0a629254
Anton Komissarov <anton.kom@hotmail.com>
2018-12-16 00:23:38 +0200

    Introduce JUnit integration test

--- src/core/src/test/com/antonkom/reportoro/ReportoroApplicationTests.kt
+++ src/core/src/test/com/antonkom/reportoro/ReportoroApplicationTests.kt
@@ -1,17 +1,25 @@
 package com.antonkom.reportoro
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.context.junit4.SpringRunner
+import java.net.URI
 
 @ExtendWith(SpringExtension::class)
-@SpringBootTest
-class BlogApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
-    fun contextLoads() {
+    fun `Assert blog page title, content and status code`() {
+        val entity = restTemplate.getForEntity<String>(URI.create("/"), String::class.java)
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Day</h1>")
     }
-
 }
